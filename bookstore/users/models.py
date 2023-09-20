e0352bdd30ad3c47a92aafc1f400a39af1a70c12
Гаяне <gayanev.stepanyan@yandex.ru>
2023-09-20 11:23:54 +0300

    profile changes

--- bookstore/users/models.py
+++ bookstore/users/models.py
@@ -35,25 +35,6 @@ def get_image_filename(instance, filename):
     slug = slugify(name)
     return f"avatars/{slug}-{filename}"
 
-    # def post(self, request):
-    #     serializer = PasswordChangeSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-        
-    #     password = request.data.get('password')
-    #     new_password = serializer.validated_data['new_password']
-    #     confirm_password = serializer.validated_data['confirm_password']
-        
-    #     if new_password != confirm_password:
-    #         return Response({'error': 'Passwords do not match'}, status=400)
-        
-    #     user = request.user 
-    #     user.set_password(new_password)
-    #     user.save()
-        
-    #     return Response({'message': 'Password changed successfully'})
-
-
-
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to=get_image_filename)
@@ -62,6 +43,6 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.email
 
-    @property
-    def filename(self):
-        return os.path.basename(self.image.name)
+    # @property
+    # def filename(self):
+    #     return os.path.basename(self.image.name)
